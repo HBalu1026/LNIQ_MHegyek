d31c88d33ffb52b45cfa01eff26969a87835d868
HBalu1026 <71315012+HBalu1026@users.noreply.github.com>
2023-05-18 14:20:15 +0200

    Add files via upload

---
+++ ConsoleApp5/ConsoleApp5/HegyCsucs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class HegyCsucs
+    {
+        string hegycsucs;
+        string hegyseg;
+        int magassag;
+
+        public HegyCsucs(string hegycsucs, string hegyseg, int magassag)
+        {
+            this.hegycsucs = hegycsucs;
+            this.hegyseg = hegyseg;
+            this.magassag = magassag;
+        }
+
+        public string Hegycsucs { get => hegycsucs; set => hegycsucs = value; }
+        public string Hegyseg { get => hegyseg; set => hegyseg = value; }
+        public int Magassag { get => magassag; set => magassag = value; }
+    }
+}
---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using ConsoleApp5;
+using System.IO;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<HegyCsucs> list = new List<HegyCsucs>();
+            string fajlNev = "hegyekMo.tyt";
+            StreamReader sr = new StreamReader(fajlNev);
+            sr.ReadLine();
+
+            while (!sr.EndOfStream)
+            {
+                string sor = sr.ReadLine();
+                HegyCsucs elem = new HegyCsucs(sor.Split(";")[0], sor.Split(";")[1], Convert.ToInt32(sor.Split(";")[2]));
+                list.Add(elem);
+            }
+            sr.Close();
+
+            //3
+            Console.WriteLine("Hegycsúcsok száma: " + list.Count() + "db");
+
+            //4
+            double ossz = 0;
+            for (int i = 0; i < list.Count(); i++)
+            {
+                ossz += list[i].Magassag;
+            }
+
+            Console.WriteLine("Hegycsúcsok átlagos magassága: " + ossz / list.Count() + "m");
+        }
+    }
+}
